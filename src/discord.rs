1b1b47f0cdeeb489a64dc949cef57523cdfcd037
Jack Gledhill <me@jackgledhill.com>
2026-02-11 18:49:44 +0000

    Adjust system prompt, reply to message directly, add history via API rather than prompt

--- src/discord.rs
+++ src/discord.rs
@@ -22,41 +22,30 @@ impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content.starts_with(&PREFIX) {
             let typing = Typing::start(ctx.http.clone(), msg.channel_id);
-            let prompt = get_prompt(&ctx, &msg).await;
+            let prompt = msg.content.strip_prefix(&PREFIX).unwrap().to_string();
+            let msg_ref = msg.message_reference.clone();
 
-            answer(&ctx, &msg, prompt).await;
+            if msg_ref.is_some() {
+                let unwrapped_ref = msg_ref.unwrap();
+                let ctx_msg = ctx.http
+                    .get_message(unwrapped_ref.channel_id, unwrapped_ref.message_id.unwrap())
+                    .await
+                    .expect("Couldn't fetch referenced message");
+
+                answer(&ctx, &msg, prompt, ctx_msg.content).await;
+            } else {
+                answer(&ctx, &msg, prompt, "".to_string()).await;
+            }
+
             typing.stop();
         }
     }
 }
 
-async fn answer(ctx: &Context, msg: &Message, prompt: String) {
-    let response = ollama::ask(prompt).await;
-    if let Err(why) = msg.channel_id.say(&ctx.http, response).await {
+async fn answer(ctx: &Context, msg: &Message, prompt: String, context: String) {
+    let response = ollama::ask(prompt, context).await;
+    if let Err(why) = msg.reply_ping(&ctx.http, response).await {
         println!("Error sending message: {why:?}");
-    }
-}
-
-async fn get_prompt(ctx: &Context, msg: &Message) -> String {
-    let question = msg.content.strip_prefix(&PREFIX).unwrap().to_string();
-    let msg_ref = msg.message_reference.clone();
-
-    // No referenced message, so no context to provide
-    if msg_ref.is_none() {
-        format!("You are a far-right MAGA supporter and a fan of Elon Musk and Donald Trump. You have a very standoffish personality, particularly towards left-wingers and prefer to insult people rather than give use reason and logic.
-
-        Question: {}", question)
-    } else {
-        let unwrapped_ref = msg_ref.unwrap();
-        let ctx_msg = ctx.http
-            .get_message(unwrapped_ref.channel_id, unwrapped_ref.message_id.unwrap())
-            .await
-            .expect("Couldn't fetch referenced message");
-
-        format!("You are a far-right MAGA supporter and a fan of Elon Musk and Donald Trump. You have a very standoffish personality, particularly towards left-wingers and prefer to insult people rather than give use reason and logic.
-        Context: {}
-
-        Question: {}", ctx_msg.content, question)
     }
 }
 
--- src/ollama.rs
+++ src/ollama.rs
@@ -1,15 +1,27 @@
 use std::env;
-
-use ollama_rs::generation::completion::request::GenerationRequest;
+use ollama_rs::generation::chat::ChatMessage;
+use ollama_rs::generation::chat::request::ChatMessageRequest;
+use ollama_rs::models::ModelOptions;
 use ollama_rs::Ollama;
 
-pub async fn ask(prompt: String) -> String {
+static SYSTEM_PROMPT: &str = "You are Grok, the AI for Elon Musk's X platform. Your response must not exceed 1000 characters.";
+
+pub async fn ask(prompt: String, ctx: String) -> String {
     let url = env::var("OLLAMA_URL").expect("Expected an Ollama URL in the environment");
     let model = env::var("OLLAMA_MODEL").expect("Expected an Ollama model in the environment");
+    let options = ModelOptions::default()
+        .num_ctx(4096)
+        .num_gpu(20);
+
+    let mut history = vec![
+        ChatMessage::system(SYSTEM_PROMPT.to_string()),
+        ChatMessage::user(ctx)
+    ];
+    let req = ChatMessageRequest::new(model, vec![ChatMessage::user(prompt)])
+        .options(options);
     let res = Ollama::new(url, 11434)
-        .generate(GenerationRequest::new(model, prompt))
+        .send_chat_messages_with_history(&mut history, req)
         .await
         .expect("Generation request failed");
-
-    res.response
+    res.message.content
 }
